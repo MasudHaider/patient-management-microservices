ba12f0df32c24712bd2b44a41cec5e70fa1279d7
Masud Haider <masud6563@gmail.com>
2025-11-20 10:04:05 +0200

    auth negative integration test

--- integration-tests/src/test/java/AuthIntegrationTest.java
+++ integration-tests/src/test/java/AuthIntegrationTest.java
@@ -34,4 +34,22 @@ public class AuthIntegrationTest {
 
         System.out.println("Generated token: " + response.jsonPath().getString("token"));
     }
+
+    @Test
+    public void shouldReturnUnauthorizedOnInvalidLogin(){
+        String loginPayload = """
+                {
+                  "email": "invalid_user@test.com",
+                  "password": "password_wrong"
+                }
+                """;
+
+        given()
+                .contentType("application/json")
+                .body(loginPayload)
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(401);
+    }
 }
